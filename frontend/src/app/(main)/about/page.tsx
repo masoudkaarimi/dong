688a3af1ae54b1786327e9f30d91de8c947b3cb4
Masoud Karimi <masoud.develop@gmail.com>
2023-10-28 21:56:08 +0330

    About and Contact page added

---
+++ frontend/src/app/(main)/about/page.tsx
@@ -0,0 +1,9 @@
+import Heading from "@/components/heading";
+
+export default function AboutPage() {
+   return (
+      <>
+         <Heading className="text-center my-8 border-b-0">About Page</Heading>
+      </>
+   );
+}
---
+++ frontend/src/app/(main)/contact/page.tsx
@@ -0,0 +1,9 @@
+import Heading from "@/components/heading";
+
+export default function ContactPage() {
+   return (
+      <>
+         <Heading className="text-center my-8 border-b-0">Contact Page</Heading>
+      </>
+   );
+}
